e769d14178d22438469c3c0c95de259c311430d2
negu63 <negu6633@gmail.com>
2022-08-16 22:07:02 +0900

    Edit Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 function App() {
   return (
     <>
-    <h3>Test</h3>
+      <h3>Store secp256k1 private key in browser</h3>
+      <hr />
+      <br />
       <div>
         <div>Public key</div>
         <div>-</div>
         <div>Private key</div>
         <div>-</div>
+        <br />
         <input type="button" value="Generate ECDSA Key" />
-        <hr/>
+        <br />
+        <br />
+        <br />
         <div>Salt</div>
         <div>-</div>
         <div>Symmetric key</div>
         <div>-</div>
         <div>Encrypted private key</div>
         <div>-</div>
+        <br />
         <label htmlFor="password">
           <div>Password</div>
           <input type="password" name="password" id="password" />
