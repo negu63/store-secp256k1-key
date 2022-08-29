45c2f6e82c8873eca0d9e78ed4bd833a9f44956e
negu63 <negu6633@gmail.com>
2022-08-29 22:53:04 +0900

    Modify password validation logic

--- src/recoil/selectors/validationSelector.ts
+++ src/recoil/selectors/validationSelector.ts
@@ -1,13 +1,23 @@
 import { selector } from "recoil";
-import { $privateKey } from "../atoms/ecdsaAtom";
+import { $publicKey } from "../atoms/ecdsaAtom";
 import { $decryptedPrivateKey } from "../atoms/validationAtom";
+import * as secp from "@noble/secp256k1";
 
 const $isValidPassword = selector({
   key: "isValidPassword",
   get: ({ get }) => {
     const decryptedPrivateKey = get($decryptedPrivateKey);
-    const privateKey = get($privateKey);
-    return decryptedPrivateKey === privateKey;
+    const publicKey = get($publicKey);
+
+    try {
+      const decryptedPublicKey = Buffer.from(
+        secp.getPublicKey(Buffer.from(decryptedPrivateKey, "hex"), true)
+      ).toString("hex");
+      
+      return decryptedPublicKey === publicKey;
+    } catch {
+      return false;
+    }
   },
 });
 
